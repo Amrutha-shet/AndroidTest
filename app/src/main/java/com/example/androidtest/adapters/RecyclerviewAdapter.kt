b077fbc94d68e236959b63c62b64260ff7ecc409
Amrutha <sshetamrutha@gmail.com>
2019-07-05 11:32:08 +0530

    Lint Fixed

--- app/src/main/java/com/example/androidtest/adapters/RecyclerviewAdapter.kt
+++ app/src/main/java/com/example/androidtest/adapters/RecyclerviewAdapter.kt
@@ -2,25 +2,22 @@ package com.example.androidtest.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidtest.databinding.RecyclerviewItemBinding
 import com.example.androidtest.model.dataModel.DataModelItem
-import com.example.androidtest.viewmodels.BaseViewModel
 
 
-class RecyclerviewAdapter(var apiResponse: MutableList<DataModelItem>? = null) :
+
+class RecyclerviewAdapter(private var apiResponse: MutableList<DataModelItem>? = null) :
     RecyclerView.Adapter<RecyclerviewAdapter.RecyclerViewholder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewholder {
 
 
-        val inflater = LayoutInflater.from(parent?.context)
+        val inflater = LayoutInflater.from(parent.context)
         val binding = RecyclerviewItemBinding.inflate(inflater, parent, false)
-        var viewholder = RecyclerViewholder(binding)
-
-        return viewholder
+        return RecyclerViewholder(binding)
 
 
     }
@@ -34,16 +31,16 @@ class RecyclerviewAdapter(var apiResponse: MutableList<DataModelItem>? = null) :
     }
 
     override fun getItemCount(): Int {
-        if (apiResponse != null && apiResponse!!.size > 0) {
-            return apiResponse!!.size
+        return if (apiResponse != null && apiResponse!!.size > 0) {
+            apiResponse!!.size
         } else {
-            return 0
+            0
         }
     }
 
 
     class RecyclerViewholder(bindingItem: RecyclerviewItemBinding) : RecyclerView.ViewHolder(bindingItem.root) {
-        var binding: RecyclerviewItemBinding? = null
+       private var binding: RecyclerviewItemBinding? = null
 
         init {
             binding = bindingItem
